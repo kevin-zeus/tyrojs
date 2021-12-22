e14d93f73d0c8493ce31caeeb8e9835ab0b510d0
zengkaiwen <zengkaiwen@zengkaiwendeMacBook-Pro.local>
2021-12-22 22:00:02 +0800

    init: 初始化

---
+++ src/utils/string.ts
@@ -0,0 +1,32 @@
+/**
+ * 判断给定字符串是否是浮点或整型数字
+ * @param str 给定的字符串
+ * @returns 
+ */
+export function isNumberic(str: string): boolean {
+  let trimedStr = str.trim();
+  return !isNaN(Number(trimedStr)) && /[+-]?([0-9]*[.])?[0-9]+/.test(trimedStr);
+}
+
+/**
+ * 判断给定字符串是不是true或false
+ * @param str 给定的字符串
+ * @returns 
+ */
+export function isBoolean(str: string): boolean {
+  let trimedStr = str.trim();
+  return (trimedStr === 'true') || (trimedStr === 'false');
+}
+
+/**
+ * 将字符串转换为对应16进制值
+ * @param str 给定的字符串
+ * @returns 
+ */
+export function toHex(str: string): string {
+  let res: string = '', c: number = 0;
+  while (c < str.length) {
+    res += str.charCodeAt(c++).toString(16);
+  }
+  return res;
+}
